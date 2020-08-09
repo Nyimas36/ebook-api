d8773bcc2dc2a35110392cdaf08dc19c2e56fda8
Nyimas36 <reikaoru4509@gmail.com>
2020-08-09 14:17:58 +0700

    add AuthController.php

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function me(){
+    	return "35_Nyimas Ananda Putri M_XII RPL 3";
+    }
+}
